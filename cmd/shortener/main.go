22821afe959a95ba8f20d8e2fe61879e06d9600f
Novikov Dmitriy <dn@aube.work>
2025-02-07 23:58:18 +0300

    add chi router into main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,21 +5,23 @@ import (
 	"net/http"
 
 	"github.com/aube/url-shortener/internal/app/handlers"
+	"github.com/go-chi/chi/v5"
 )
 
 const portNumber = "8080"
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, http.HandlerFunc(handlers.HandlerRoot))
-	mux.HandleFunc(`/{id}`, http.HandlerFunc(handlers.HandlerId))
+	r := chi.NewRouter()
+
+	r.Post("/", http.HandlerFunc(handlers.HandlerRoot))
+	r.Get("/{id}", http.HandlerFunc(handlers.HandlerId))
 
 	// empty handler for automatic browser favicon request
-	mux.HandleFunc(`/favicon.ico`, http.HandlerFunc(handlers.HandlerEmpty))
+	r.Get("/favicon.ico", http.HandlerFunc(handlers.HandlerEmpty))
 
 	fmt.Println("Server starting at:", portNumber)
 
-	err := http.ListenAndServe(":"+portNumber, mux)
+	err := http.ListenAndServe(":"+portNumber, r)
 
 	if err != nil {
 		fmt.Println("Error starting server:", err)
