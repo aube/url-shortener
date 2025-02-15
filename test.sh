bec79fac7e7dadb553c087b60cd0254bbf644e2a
Novikov Dmitriy <dn@aube.work>
2025-02-15 09:38:46 +0300

    test.sh update

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-./shortenertest -test.v -test.run=^TestIteration4$ -binary-path=cmd/shortener/shortener
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+ITERATION=${BRANCH: -1}
+
+./shortenertest -test.v -test.run=^TestIteration$ITERATION$ -binary-path=cmd/shortener/shortener -server-port=8080
+
 
