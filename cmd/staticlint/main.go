d10bac52661ef257f98c117840710ac775648e06
Novikov Dmitriy <dn@aube.work>
2025-05-31 19:23:04 +0300

    iter19: os.Exit

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"go/ast"
-	"go/types"
 	"os"
 	"path/filepath"
 
@@ -13,6 +11,8 @@ import (
 	"golang.org/x/tools/go/analysis/passes/shadow"
 	"golang.org/x/tools/go/analysis/passes/structtag"
 	"honnef.co/go/tools/staticcheck"
+
+	"github.com/aube/url-shortener/internal/staticlint/slerrors"
 )
 
 // Config — имя файла конфигурации.
@@ -38,6 +38,7 @@ func main() {
 	}
 	mychecks := []*analysis.Analyzer{
 		ErrCheckAnalyzer,
+		ErrRunErrOSExit,
 		printf.Analyzer,
 		shadow.Analyzer,
 		structtag.Analyzer,
@@ -48,125 +49,24 @@ func main() {
 	}
 	// добавляем анализаторы из staticcheck, которые указаны в файле конфигурации
 	for _, v := range staticcheck.Analyzers {
-		if checks[v.Analyzer.Name] {
+		if checks["*"] || checks[v.Analyzer.Name] {
 			mychecks = append(mychecks, v.Analyzer)
 		}
 	}
+
 	multichecker.Main(
 		mychecks...,
 	)
 }
 
-var errorType = types.
-	// ищем тип error в области вилимости Universe, в котором находятся
-	// все предварительно объявленные объекты Go
-	Universe.Lookup("error").
-	// получаем объект, представляющий тип error
-	Type().
-	// получаем тип, при помощи которого определен тип error (см. https://go.dev/ref/spec#Underlying_types);
-	// мы знаем, что error определен как интерфейс, приведем полученный объект к этому типу
-	Underlying().(*types.Interface)
-
-func isErrorType(t types.Type) bool {
-	// проверяем, что t реализует интерфейс, при помощи которого определен тип error,
-	// т.е. для типа t определен метод Error() string
-	return types.Implements(t, errorType)
-}
-
-func resultErrors(pass *analysis.Pass, call *ast.CallExpr) []bool {
-	switch t := pass.TypesInfo.Types[call].Type.(type) {
-	case *types.Named: // возвращается значение
-		return []bool{isErrorType(t)}
-	case *types.Pointer: // возвращается указатель
-		return []bool{isErrorType(t)}
-	case *types.Tuple: // возвращается несколько значений
-		s := make([]bool, t.Len())
-		for i := range t.Len() {
-			switch mt := t.At(i).Type().(type) {
-			case *types.Named:
-				s[i] = isErrorType(mt)
-			case *types.Pointer:
-				s[i] = isErrorType(mt)
-			}
-		}
-		return s
-	}
-	return []bool{false}
-}
-
-// isReturnError возвращает true, если среди возвращаемых значений есть ошибка.
-func isReturnError(pass *analysis.Pass, call *ast.CallExpr) bool {
-	for _, isError := range resultErrors(pass, call) {
-		if isError {
-			return true
-		}
-	}
-	return false
-}
-
 var ErrCheckAnalyzer = &analysis.Analyzer{
 	Name: "errcheck",
 	Doc:  "check for unchecked errors",
-	Run:  run,
+	Run:  slerrors.RunErrUnchecked,
 }
 
-func run(pass *analysis.Pass) (any, error) {
-	expr := func(x *ast.ExprStmt) {
-		// проверяем, что выражение представляет собой вызов функции,
-		// у которой возвращаемая ошибка никак не обрабатывается
-		if call, ok := x.X.(*ast.CallExpr); ok {
-			if isReturnError(pass, call) {
-				pass.Reportf(x.Pos(), "expression returns unchecked error")
-			}
-		}
-	}
-	tuplefunc := func(x *ast.AssignStmt) {
-		// рассматриваем присваивание, при котором
-		// вместо получения ошибок используется '_'
-		// a, b, _ := tuplefunc()
-		// проверяем, что это вызов функции
-		if call, ok := x.Rhs[0].(*ast.CallExpr); ok {
-			results := resultErrors(pass, call)
-			for i := range len(x.Lhs) {
-				// перебираем все идентификаторы слева от присваивания
-				if id, ok := x.Lhs[i].(*ast.Ident); ok && id.Name == "_" && results[i] {
-					pass.Reportf(id.NamePos, "assignment with unchecked error")
-				}
-			}
-		}
-	}
-	errfunc := func(x *ast.AssignStmt) {
-		// множественное присваивание: a, _ := b, myfunc()
-		// ищем ситуацию, когда функция справа возвращает ошибку,
-		// а соответствующий идентификатор слева равен '_'
-		for i := range len(x.Lhs) {
-			if id, ok := x.Lhs[i].(*ast.Ident); ok {
-				// вызов функции справа
-				if call, ok := x.Rhs[i].(*ast.CallExpr); ok {
-					if id.Name == "_" && isReturnError(pass, call) {
-						pass.Reportf(id.NamePos, "assignment with unchecked error")
-					}
-				}
-			}
-		}
-	}
-	for _, file := range pass.Files {
-		// функцией ast.Inspect проходим по всем узлам AST
-		ast.Inspect(file, func(node ast.Node) bool {
-			switch x := node.(type) {
-			case *ast.ExprStmt: // выражение
-				expr(x)
-			case *ast.AssignStmt: // оператор присваивания
-				// справа одно выражение x,y := myfunc()
-				if len(x.Rhs) == 1 {
-					tuplefunc(x)
-				} else {
-					// справа несколько выражений x,y := z,myfunc()
-					errfunc(x)
-				}
-			}
-			return true
-		})
-	}
-	return nil, nil
+var ErrRunErrOSExit = &analysis.Analyzer{
+	Name: "osexit",
+	Doc:  "check for os.Exit",
+	Run:  slerrors.RunErrOSExit,
 }
---
+++ internal/staticlint/slerrors/errunchecked.go
@@ -0,0 +1,68 @@
+package slerrors
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+func RunErrUnchecked(pass *analysis.Pass) (any, error) {
+	expr := func(x *ast.ExprStmt) {
+		// проверяем, что выражение представляет собой вызов функции,
+		// у которой возвращаемая ошибка никак не обрабатывается
+		if call, ok := x.X.(*ast.CallExpr); ok {
+			if isReturnError(pass, call) {
+				pass.Reportf(x.Pos(), "expression returns unchecked error")
+			}
+		}
+	}
+	tuplefunc := func(x *ast.AssignStmt) {
+		// рассматриваем присваивание, при котором
+		// вместо получения ошибок используется '_'
+		// a, b, _ := tuplefunc()
+		// проверяем, что это вызов функции
+		if call, ok := x.Rhs[0].(*ast.CallExpr); ok {
+			results := resultErrors(pass, call)
+			for i := range len(x.Lhs) {
+				// перебираем все идентификаторы слева от присваивания
+				if id, ok := x.Lhs[i].(*ast.Ident); ok && id.Name == "_" && results[i] {
+					pass.Reportf(id.NamePos, "assignment with unchecked error")
+				}
+			}
+		}
+	}
+	errfunc := func(x *ast.AssignStmt) {
+		// множественное присваивание: a, _ := b, myfunc()
+		// ищем ситуацию, когда функция справа возвращает ошибку,
+		// а соответствующий идентификатор слева равен '_'
+		for i := range len(x.Lhs) {
+			if id, ok := x.Lhs[i].(*ast.Ident); ok {
+				// вызов функции справа
+				if call, ok := x.Rhs[i].(*ast.CallExpr); ok {
+					if id.Name == "_" && isReturnError(pass, call) {
+						pass.Reportf(id.NamePos, "assignment with unchecked error")
+					}
+				}
+			}
+		}
+	}
+	for _, file := range pass.Files {
+		// функцией ast.Inspect проходим по всем узлам AST
+		ast.Inspect(file, func(node ast.Node) bool {
+			switch x := node.(type) {
+			case *ast.ExprStmt: // выражение
+				expr(x)
+			case *ast.AssignStmt: // оператор присваивания
+				// справа одно выражение x,y := myfunc()
+				if len(x.Rhs) == 1 {
+					tuplefunc(x)
+				} else {
+					// справа несколько выражений x,y := z,myfunc()
+					errfunc(x)
+				}
+			}
+			return true
+		})
+	}
+	return nil, nil
+}
